3fc9fe0266d4befc672fe855a53d3145cace13a7
fotistsiou <fotis.tsioumas@outlook.com>
2025-04-11 22:25:35 +0300

    Step 4

--- src/step_4/Main.java
+++ src/step_4/Main.java
@@ -1,5 +1,9 @@
 package step_4;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Like a clockwork
  * ----------------
@@ -41,4 +45,130 @@ package step_4;
  */
 
 public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+    static volatile boolean inSystemState = false;
+    static volatile boolean programRunning = true;
+    static int timeSinceStartup = 0;
+    static int roads;
+    static int interval;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the traffic management system!");
+
+        System.out.print("Input the number of roads: ");
+        while (true) {
+            try {
+                roads = scanner.nextInt();
+                if (roads > 0) {
+                    break;
+                }
+                System.out.print("Incorrect input. Try again: ");
+            } catch (Exception e) {
+                System.out.print("Incorrect input. Try again: ");
+            } finally {
+                scanner.nextLine();
+            }
+        }
+
+        System.out.print("Input the interval: ");
+        while (true) {
+            try {
+                interval = scanner.nextInt();
+                if (interval > 0) {
+                    break;
+                }
+                System.out.print("Incorrect input. Try again: ");
+            } catch (Exception e) {
+                System.out.print("Incorrect input. Try again: ");
+            } finally {
+                scanner.nextLine();
+            }
+        }
+
+        QueueThread queueThread = new QueueThread();
+        queueThread.setName("QueueThread");
+        queueThread.start();
+
+        while (true) {
+            clearConsole();
+            System.out.print(
+                    """
+                    Menu:
+                    1. Add road
+                    2. Delete road
+                    3. Open system
+                    0. Quit
+                    """
+            );
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine();
+
+                if (choice >= 0 && choice <= 3) {
+                    switch (choice) {
+                        case 1 -> {
+                            System.out.println("Road added. Press \"Enter\" to open menu.");
+                            scanner.nextLine();
+                        }
+                        case 2 -> {
+                            System.out.println("Road deleted. Press \"Enter\" to open menu.");
+                            scanner.nextLine();
+                        }
+                        case 3 -> {
+                            inSystemState = true;
+                            while (true) {
+                                String input = scanner.nextLine();
+                                if (input.isEmpty()) {
+                                    inSystemState = false;
+                                    break;
+                                }
+                            }
+                        }
+                        case 0 -> {
+                            System.out.println("Buy!");
+                            programRunning = false;
+                            queueThread.join(); // wait to terminate
+                            return;
+                        }
+                    }
+                } else {
+                    System.out.println("Incorrect option. Try again");
+                    scanner.nextLine();
+                }
+            } catch (Exception e) {
+                System.out.println("Incorrect option. Try again");
+                scanner.nextLine();
+                scanner.nextLine();
+            }
+        }
+    }
+
+    static void clearConsole() {
+        try {
+            var clearCommand = System.getProperty("os.name").contains("Windows")
+                    ? new ProcessBuilder("cmd", "/c", "cls")
+                    : new ProcessBuilder("clear");
+            clearCommand.inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException ignored) {}
+    }
+
+    static class QueueThread extends Thread {
+        @Override
+        public void run() {
+            while (programRunning) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                    timeSinceStartup++;
+                    if (inSystemState) {
+                        System.out.println("! " + timeSinceStartup + "s. have passed since system startup !");
+                        System.out.println("! Number of roads: " + roads + " !");
+                        System.out.println("! Interval: " + interval + " !");
+                        System.out.println("! Press \"Enter\" to open menu !");
+                    }
+                } catch (InterruptedException ignored) {}
+            }
+        }
+    }
 }
