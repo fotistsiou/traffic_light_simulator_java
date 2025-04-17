0b9c6a9dc97e230ed530d6bd22a719d1c1409365
fotistsiou <fotis.tsioumas@outlook.com>
2025-04-17 22:11:41 +0300

    Step 6

--- src/step_6/Main.java
+++ src/step_6/Main.java
@@ -1,5 +1,8 @@
 package step_6;
 
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Red, yellow, green
  * ------------------
@@ -72,4 +75,197 @@ package step_6;
  */
 
 public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+
+    static int numberOfRoads;
+    static int interval;
+
+    // Thread Counter State Variables
+    static volatile boolean inSystemState = false;
+    static volatile boolean programRunning = true;
+    static int timeSinceStartup = 0;
+
+    // Circular Queue Implementation Variables
+    static String[] roadNames;
+    static int[] roadIntervals;
+    static int roadIndexRear = 0;
+    static int roadIndexFront = 0;
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the traffic management system!");
+
+        Main.numberOfRoads = Main.definePositiveInteger("Input the number of roads");
+        Main.roadNames = new String[Main.numberOfRoads];
+
+        Main.interval = Main.definePositiveInteger("Input the interval");
+        Main.roadIntervals = new int[Main.numberOfRoads];
+
+        QueueThread queueThread = new QueueThread();
+        queueThread.setName("QueueThread");
+        queueThread.start();
+
+        while (true) {
+            System.out.print(
+                    """
+                    Menu:
+                    1. Add road
+                    2. Delete road
+                    3. Open system
+                    0. Quit
+                    """
+            );
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+                scanner.nextLine(); // Clear the buffer (consume leftover newline)
+                if (choice >= 0 && choice <= 3) {
+                    switch (choice) {
+                        case 1 -> {
+                            System.out.print("Input road name: ");
+                            String input = Main.scanner.nextLine();
+
+                            if (Main.roadNames[Main.roadIndexRear] == null) {
+                                Main.roadNames[Main.roadIndexRear] = input; // Add road name to the roadNames queue
+                                Main.roadIntervals[Main.roadIndexRear] = Main.interval;
+                                System.out.println(Main.roadNames[Main.roadIndexRear] + " added. Press \"Enter\" to open menu.");
+
+                                // Update rear index for circular queue:
+                                // Calculates the next position using modulo to wrap around the array.
+                                // Formula: nextRear = (currentRear + 1) % sizeOfArray
+                                Main.roadIndexRear = (Main.roadIndexRear + 1) % Main.numberOfRoads;
+                            } else {
+                                System.out.println("Queue is full. Press \"Enter\" to open menu.");
+                            }
+                            Main.scanner.nextLine(); // Wait for user to press Enter
+                        }
+                        case 2 -> {
+                            if (Main.roadNames[Main.roadIndexFront] != null) {
+                                System.out.println(Main.roadNames[Main.roadIndexFront] + " deleted. Press \"Enter\" to open menu.");
+                                Main.roadNames[Main.roadIndexFront] = null;
+
+                                // Update front index for circular queue:
+                                // Calculates the next position using modulo to wrap around the array.
+                                // Formula: nextFront = (currentFront + 1) % sizeOfArray
+                                Main.roadIndexFront = (Main.roadIndexFront + 1) % Main.numberOfRoads;
+                            } else {
+                                System.out.println("Queue is empty. Press \"Enter\" to open menu.");
+                            }
+                            Main.scanner.nextLine(); // Wait for user to press Enter
+                        }
+                        case 3 -> {
+                            inSystemState = true; // Signal the queueThread for opening system state
+
+                            while (true) {
+                                if (scanner.nextLine().isEmpty()) { // Wait for user to press Enter
+                                    inSystemState = false; // Signal the queueThread for closing system state
+                                    break;
+                                }
+                            }
+                        }
+                        case 0 -> {
+                            System.out.println("Buy!");
+                            programRunning = false; // Signal the queueThread to stop
+                            queueThread.join(); // Wait for the queueThread to stop
+                            return; // Terminate the program
+                        }
+                    }
+                } else {
+                    System.out.println("Incorrect option. Press \"Enter\" to open menu and try again.");
+                    scanner.nextLine(); // Wait for user to press Enter
+                }
+            } catch (Exception e) {
+                System.out.println("Incorrect option. Press \"Enter\" to open menu and try again.");
+                scanner.nextLine(); // Clear the invalid input
+                scanner.nextLine(); // Wait for user to press Enter
+            }
+        }
+    }
+
+    static int definePositiveInteger(String promptType) {
+        System.out.print(promptType + ": ");
+        int promptTypeNumber;
+        while (true) {
+            try {
+                promptTypeNumber = Main.scanner.nextInt();
+                if (promptTypeNumber > 0) {
+                    break; // Valid positive integer entered
+                }
+                System.out.print("Incorrect input. Try again: ");
+            } catch (Exception e) {
+                System.out.print("Incorrect input. Try again: ");
+            } finally {
+                scanner.nextLine(); // Clear the buffer (consume leftover newline)
+            }
+        }
+        return promptTypeNumber;
+    }
+}
+
+class QueueThread extends Thread {
+    @Override
+    public void run() {
+        while (Main.programRunning) { // Keep the thread running while the program is active
+            try {
+                // Sleep for 1 second to regulate the printing interval
+                TimeUnit.SECONDS.sleep(1);
+
+                // Increment the time since the program started
+                Main.timeSinceStartup++;
+
+                int availableRoads = Math.abs(Main.roadIndexFront - Main.roadIndexRear);
+                int totalTime = Main.interval * availableRoads;
+                for (int i = 0; i < Main.numberOfRoads; i++) {
+                    if (Main.roadNames[i] == null) {
+                        continue;
+                    }
+                    Main.roadIntervals[i]--;
+                    if (Main.roadIntervals[i] < -totalTime) {
+                        Main.roadIntervals[i] = Main.interval;
+                    }
+                }
+
+                // If the system is in an active state, print relevant information
+                if (Main.inSystemState) {
+                    // Print Initial Standard Message
+                    System.out.println("! " + Main.timeSinceStartup + "s. have passed since system startup !");
+                    System.out.println("! Number of roads: " + Main.numberOfRoads + " !");
+                    System.out.println("! Interval: " + Main.interval + " !");
+
+                    // TODO: TESTING
+                    System.out.println("availableRoads: " + availableRoads);
+                    System.out.println("totalTime: " + totalTime);
+
+                    // Print Road Messages
+                    System.out.println();
+                    for (int i = 0; i < Main.numberOfRoads; i++) {
+                        if (Main.roadNames[i] != null) {
+                            if (Main.roadIntervals[i] >= 0) {
+                                System.out.println(
+                                        Main.roadNames[i] +
+                                                "\u001B[32m" +
+                                                " will be open for " +
+                                                Main.roadIntervals[i] + "s." +
+                                                "\u001B[0m"
+                                );
+                            } else {
+                                System.out.println(
+                                        Main.roadNames[i] +
+                                                "\u001B[31m" +
+                                                " will be closed for " +
+                                                Math.abs(Main.roadIntervals[i]) + "s." +
+                                                "\u001B[0m"
+                                );
+                            }
+                        }
+                    }
+                    System.out.println();
+
+                    // Print End Standard Message
+                    System.out.println("! Press \"Enter\" to open menu !");
+                }
+            } catch (InterruptedException ignored) {}
+        }
+    }
 }
